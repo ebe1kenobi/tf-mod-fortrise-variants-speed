c03de74f38b226ca3c35f92c7d60cfdc4e33f31c
ebe1kenobi <ebe1.kenob.i@gmail.com>
2026-08-03 17:44:45 +0200

    fortrise 5

--- script/config.bat
+++ script/config.bat
@@ -6,7 +6,7 @@ set DLL_MOD_FILE_NAME=TFModFortRiseVariantSpeed.dll
 set PDB_MOD_FILE_NAME=TFModFortRiseVariantSpeed.pdb
 set MODULE_DIR_NAME=tf-mod-fortrise-variants-speed
 
-set REPO_PATH=D:\__dev\code\tf-mod-fortrise-variants-speed\
+set REPO_PATH=D:\__dev\code\FR5tf-mod-fortrise-variants-speed\
 set REPO_BUILD_PATH=%REPO_PATH%VSCode\bin\Debug\net10.0\
 set REPO_RELEASE_PATH=%REPO_PATH%release\
 set REPO_RELEASE_MOD_PATH=%REPO_RELEASE_PATH%\tf-mod-fortrise-variants-speed\
